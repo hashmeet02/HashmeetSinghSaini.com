5c10e123dc40d95abe5b86c6d4999a5672334eae
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-07-20 17:24:13 -0400

    Footer section created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='text-white font-roboto text-center mb-3'>
+        Copyright © Hashmeet Singh Saini | Made with 💖 in Toronto
+    </div>
+  )
+}
+
+export default Footer
