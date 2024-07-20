c61df3710727438a53722246dbac04a3fc98ff05
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-07-20 17:23:12 -0400

    NavBar setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+const NavBar = () => {
+  return (
+    <div>
+      <Navbar variant="light" expand="lg" fixed="top" className="bg-gradient-to-r from-black/90 via-orange-950 to-black/90 items-center font-roboto">
+        <Container>
+          <Navbar.Brand href="#home">
+            {/* <img 
+              src={logo}
+              alt="Hashmeet Logo"
+              className="h-12" // Adjust the height of the logo as needed
+            /> */}
+            <div className='text-3xl text-orange-400'>HASHMEET S. SAINI</div>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="text-white" />
+          <Navbar.Collapse id="basic-navbar-nav" className="p-6 rounded-3xl">
+            <Nav className="ml-auto">
+              <Nav.Link href="#toolkit" className="text-white !text-2xl hover:!text-orange-400">Toolkit</Nav.Link>
+              <Nav.Link href="#works" className="text-white !text-2xl hover:!text-orange-400">Works</Nav.Link>
+              <Nav.Link href="#contact" className="text-white !text-2xl hover:!text-orange-400">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
+
