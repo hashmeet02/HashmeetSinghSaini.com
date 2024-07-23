a8fbc99aa03da6e9351f92c1b2ae105a9f59641f
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-07-23 09:02:03 -0400

    animation added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Navbar from "./components/NavBar";
+import React from "react";
 import { Helmet } from "react-helmet";
+import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Works from "./components/Works";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import FadeInSection from "./components/FadeInSection"; // Adjust the import path as necessary
 
 function App() {
   return (
@@ -14,13 +17,18 @@ function App() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet" />
       </Helmet>
-      <Navbar />
-      <Home />
-      <Skills />
-      <Works/>
-      <Contact/>
+      <NavBar />
+      <Home />      
+      <FadeInSection>
+        <Skills />
+      </FadeInSection>
+      <FadeInSection>
+        <Works />
+      </FadeInSection>
+      <FadeInSection>
+        <Contact />
+      </FadeInSection>
       <Footer />
-
     </div>
   );
 }
