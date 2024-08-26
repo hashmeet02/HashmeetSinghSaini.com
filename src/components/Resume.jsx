2620053a18a07b37f788e0bbe0a54b8f38809b67
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-08-26 16:13:17 -0400

    made entire resume button link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -16,17 +16,19 @@ const Resume = () => {
   <div
     className='absolute inset-0 flex items-center justify-center z-10'
   >
+    <a href="hashmeet_singh_saini_resume.pdf" target="_blank" className='flex items-center justify-center'>
     <div
       className='border-8 bg-blue-500/50 border-dashed backdrop-blur-md rounded-full w-48 h-48 md:w-72 md:h-72 flex items-center justify-center transition transform hover:scale-150 '
     >
-      <a href="hashmeet_singh_saini_resume.pdf" target="_blank" className='flex items-center justify-center'>
+      
         <img
           src={cv}
           alt="download resume"
           className='w-24 h-24 md:w-48 md:h-48'
         />
-      </a>
+      
     </div>
+    </a>
   </div>
 </div>
 
