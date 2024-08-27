7d7a154f07d85f18f8826e073ee94d6d5046d875
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-08-27 18:06:19 -0400

    using animated cursor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import NavBar from "./components/NavBar";
+import AnimatedCursor from "react-animated-cursor"
 import Home from "./components/Home";
 import Works from "./components/Works";
 import Skills from "./components/Skills";
@@ -12,6 +13,23 @@ import FadeInSection from "./components/FadeInSection"; // Adjust the import pat
 function App() {
   return (
     <div className="min-h-screen overflow-hidden">
+<AnimatedCursor
+  color="251, 146, 60"
+  innerSize={15}
+  outerSize={65}
+  innerScale={1}
+  outerScale={1.7}
+  outerAlpha={0.7}
+  innerStyle={{
+    position: 'fixed', // Ensure the cursor stays on top
+    zIndex: 10000000, // High z-index to ensure visibility
+  }}
+  outerStyle={{
+    mixBlendMode: 'difference', // Experiment with different modes
+    position: 'fixed', // Ensure the cursor stays on top
+    zIndex: 10000000, // High z-index to ensure visibility
+  }}
+/>
       <Helmet>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
